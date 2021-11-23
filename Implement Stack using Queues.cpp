eb199aa05cf726b6f8b35e32c796893c94b1b131
Raja Durai M <84493140+Raja58@users.noreply.github.com>
2021-11-23 13:02:31 +0530

    Create Implement Stack using Queues.cpp

---
+++ Implement Stack using Queues.cpp
@@ -0,0 +1,46 @@
+// <!--
+//  ============================
+//  @Author  :        raja_5
+//  @Version :        1.0
+//  @Date    :        23 Nov 2021
+//  @Detail  :        Implement Stack using Queues (LeetCode)
+//  ============================
+//  -->
+
+class MyStack {
+public:
+    
+    queue<int> q1, q2;
+    
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        while(!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        q1.push(x);
+        while(!q2.empty())
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+    }
+    
+    int pop() {
+        int result = q1.front();
+        q1.pop();
+        return result;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
