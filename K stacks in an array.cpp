955a527f31b6f87efb596234ed06d5361fe12e6d
Raja Durai M <84493140+Raja58@users.noreply.github.com>
2021-12-12 18:01:32 +0530

    Create K stacks in an array.cpp

---
+++ K stacks in an array.cpp
@@ -0,0 +1,52 @@
+// <!--
+//  ============================
+//  @Author  :        raja_5
+//  @Version :        1.0
+//  @Date    :        12 Dec 2021
+//  @Detail  :        K stacks in an array
+//  ============================
+//  -->
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct MyStack {
+ 
+    stack<int> s;
+    int min;
+ 
+void push(int x) {
+ 
+      if(s.empty() ) {
+         min=x;
+         s.push(x);
+      }
+      else if(x<=min){
+          s.push(2*x-min);
+          min=x;
+      }else{
+      s.push(x);
+      }
+   }
+ 
+int pop() {
+
+    int t=s.top();s.pop();
+    if(t<=min){
+        int res=min;
+        min=2*min-t;
+        return res;
+    }else{
+        return t;
+    }
+   }
+
+int top() {
+    int t=s.top();
+    return ((t<=min)? min : t);
+   }
+ 
+int getMin() {
+      return min;
+   }
+};
